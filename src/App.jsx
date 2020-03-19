8ce70c43d058ffb9c1987cd5b7cb1584057a89e9
Erix Alexander <erixmv022@gmail.com>
2020-03-19 12:45:50 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/*
+    ! WARNING: Using the module: hot-loader creates warnings in console.  
+*/
+import {hot} from "react-hot-loader";
+
+function App () {
+    return <div>
+        
+    </div>
+}
+
+export default hot(module)(App);
+// export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+    entry: path.resolve(__dirname, "./src/index.js"),
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    // plugins: [new webpack.HotModuleReplacementPlugin()]
+}
